d5a218db0741694cfb9625bb13529844180f4fc2
ianroddrs <ianroddrs@gmail.com>
2024-04-09 03:45:33 -0300

    criacao de template inicial

---
+++ imeireader/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ImeireaderConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'imeireader'
---
+++ imeireader/static/js/imeireader.js
@@ -0,0 +1,50 @@
+Quagga.init({
+    inputStream : {
+      name : "Live",
+      type : "LiveStream",
+      constraints: {
+        width: window.innerWidth - 25,
+        height: window.innerHeight - 80,
+      },
+      target: document.querySelector('#camera')    // Or '#yourElement' (optional)
+    },
+    decoder : {
+      readers : ["code_128_reader"],
+    }
+  }, function(err) {
+      if (err) {
+          console.log(err);
+          return
+      }
+      console.log("Initialization finished. Ready to start");
+      Quagga.start();
+  });
+
+  Quagga.onDetected(function(data){
+    console.log(data);
+    document.querySelector('#resultado').innerHTML = data.codeResult.code
+  })
+
+  Quagga.onProcessed(function(result) {
+    var drawingCtx = Quagga.canvas.ctx.overlay,
+        drawingCanvas = Quagga.canvas.dom.overlay;
+
+    if (result) {
+        if (result.boxes) {
+            drawingCtx.clearRect(0, 0, parseInt(drawingCanvas.getAttribute("width")), parseInt(drawingCanvas.getAttribute("height")));
+            result.boxes.filter(function (box) {
+                return box !== result.box;
+            }).forEach(function (box) {
+                Quagga.ImageDebug.drawPath(box, {x: 0, y: 1}, drawingCtx, {color: "green", lineWidth: 2});
+            });
+        }
+
+        if (result.box) {
+            Quagga.ImageDebug.drawPath(result.box, {x: 0, y: 1}, drawingCtx, {color: "#00F", lineWidth: 2});
+        }
+
+        if (result.codeResult && result.codeResult.code) {
+            Quagga.ImageDebug.drawPath(result.line, {x: 'x', y: 'y'}, drawingCtx, {color: 'red', lineWidth: 3});
+        }
+    }
+});
