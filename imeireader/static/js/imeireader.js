0ba63c2e74b3a80ae76a824f7d01e56391e735c6
ianroddrs <mateusroddi@gmail.com>
2024-04-09 12:11:22 -0300

    MUDANÇA DE BIBLIOTECA: QAGGUA -> HTML-QRCODE

--- imeireader/static/js/imeireader.js
+++ imeireader/static/js/imeireader.js
@@ -1,50 +1,29 @@
-Quagga.init({
-    inputStream : {
-      name : "Live",
-      type : "LiveStream",
-      constraints: {
-        width: window.innerWidth - 25,
-        height: window.innerHeight - 80,
-      },
-      target: document.querySelector('#camera')    // Or '#yourElement' (optional)
+let html5QrcodeScanner = new Html5QrcodeScanner(
+  "reader",
+  {
+    fps: 10,
+    qrbox: {
+      width: 250,
+      height: 100
     },
-    decoder : {
-      readers : ["code_128_reader"],
-    }
-  }, function(err) {
-      if (err) {
-          console.log(err);
-          return
-      }
-      console.log("Initialization finished. Ready to start");
-      Quagga.start();
-  });
+    formatsToSupport: [
+      Html5QrcodeSupportedFormats.CODE_128
+    ] 
+  },
+);
 
-  Quagga.onDetected(function(data){
-    console.log(data);
-    document.querySelector('#resultado').innerHTML = data.codeResult.code
-  })
+html5QrcodeScanner.render(
 
-  Quagga.onProcessed(function(result) {
-    var drawingCtx = Quagga.canvas.ctx.overlay,
-        drawingCanvas = Quagga.canvas.dom.overlay;
+  function onScanSuccess(decodedText, decodedResult) {
 
-    if (result) {
-        if (result.boxes) {
-            drawingCtx.clearRect(0, 0, parseInt(drawingCanvas.getAttribute("width")), parseInt(drawingCanvas.getAttribute("height")));
-            result.boxes.filter(function (box) {
-                return box !== result.box;
-            }).forEach(function (box) {
-                Quagga.ImageDebug.drawPath(box, {x: 0, y: 1}, drawingCtx, {color: "green", lineWidth: 2});
-            });
-        }
+    console.log(`Code matched = ${decodedText}`, decodedResult)
 
-        if (result.box) {
-            Quagga.ImageDebug.drawPath(result.box, {x: 0, y: 1}, drawingCtx, {color: "#00F", lineWidth: 2});
-        }
+  },
+  
+  function onScanFailure(error) {
 
-        if (result.codeResult && result.codeResult.code) {
-            Quagga.ImageDebug.drawPath(result.line, {x: 'x', y: 'y'}, drawingCtx, {color: 'red', lineWidth: 3});
-        }
-    }
-});
+    console.warn(`Code scan error = ${error}`);
+
+  }
+  
+)
